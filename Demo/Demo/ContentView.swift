c5d7629d928237d4ba330ac179398f74b74bfec7
Max Rozdobudko <max.rozdobudko@gmail.com>
2020-06-26 18:18:58 +0300

    Adds Demo project

---
+++ Demo/Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Max Rozdobudko on 6/26/20.
+//  Copyright © 2020 Max Rozdobudko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
